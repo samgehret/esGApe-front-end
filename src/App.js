c8d6f7f05e15452e81da1909af71ddce84b95674
Grant_Nathaniel_Brown <gnb225@nyu.edu>
2018-03-27 13:43:15 -0400

    made simple navbar, imported react-router-dom, made hello world for signup and login forms

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react'
 // import logo from './logo.svg';
 import './App.css'
 import Restaurant from './components/Restaurant'
-
+import Navbar from './components/Navbar'
+import SignupForm from './components/forms/SignupForm'
+import LoginForm from './components/forms/LoginForm'
+import {Route, Link, Redirect, Switch} from 'react-router-dom'
 class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
+      email: '',
+      password: '',
+      loggedIn: false,
       restaurant: []  // restaurant start from [] empty array
     }
   }
@@ -15,6 +21,10 @@ class App extends Component {
     return (
       <div className='App'>
         <div className='Main'>
+        <Navbar />
+        {/* Add Switch? */}
+          <Route path='/signup' render={() => <SignupForm />}/>
+          <Route path='/login' render={() => <LoginForm />}/>
           <div className='search'>
             <input type='text' placeholder='Search...' value={this.state.search} onChange={this.handleSearch} />
           </div>
---
+++ src/components/forms/SignupForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignupForm extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World Signup</h1>
+            </div>
+        );
+    }
+}
+
+export default SignupForm;
