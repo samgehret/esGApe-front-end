30f0637b59e12d47642dc367ec13d68ca0ec69d9
SAMUEL HAILE <samishken@gmail.com>
2018-03-27 14:27:35 -0400

    restaurant component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 // import logo from './logo.svg';
 import './App.css'
 import Restaurant from './components/Restaurant'
+//import axios from 'axios'
 
 class App extends Component {
   constructor (props) {
@@ -12,17 +13,33 @@ class App extends Component {
   }
 
   render () {
+    const restaurant = {
+      '_id': '5aba77a8952c454828cd34d5',
+      'name': 'Post Pub',
+      'address': '1422 L St NW, Washington, DC 20005',
+      'website': 'https://postpubdc.com/',
+      'drinkPrice': '7',
+      'ambiance': 'A dive bar with subpar service. Expect to wait a long time between server visits.',
+      'description': 'This is by far the closest bar to GA. Its a dive bar with poor service and random happy hour deals every night. Be careful on Friday night there are NO BEERS SPECIALS. Feel like drinks here should be less expensive than they are...',
+      'crowds': 'Fills up for lunch and for happy hour. Can be difficult to find a seat',
+      'distance': '1 block from GA',
+      'deals': 'Friday after work is Absolute drinks for a reduced rate'
+    }
+
     return (
       <div className='App'>
         <div className='Main'>
           <div className='search'>
             <input type='text' placeholder='Search...' value={this.state.search} onChange={this.handleSearch} />
           </div>
-          <div className='restaurant'>
+          <div>
+            <Restaurant restaurant={restaurant} />
+          </div>
+          {/* <div className='restaurant'>
             {this.state.restaurant.map((restaurant) => {
               return <Restaurant key={restaurant.name} />
             })}
-          </div>
+          </div> */}
         </div>
       </div>
     )
--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react'
+import './Restaurant.css'
 
 class Restaurant extends Component {
   render () {
-    const style = { backgroundImage: "url('" + this.props.restaurant.imageUrl + "')"}
-
-    const name = this.props.restaurant.name + '$$$' + ' - ' + this.props.restaurant.name
-
+    const restaurant = this.props.restaurant.name + ' $' + this.props.restaurant.drinkPrice + ' - ' + this.props.restaurant.distance
+    const address = this.props.restaurant.address
+    const feedback = this.props.restaurant.description
+    const ambiance = this.props.restaurant.ambiance
+    const crowds = this.props.restaurant.crowds
+    const deals = this.props.restaurant.deals
     return (
 
-      <div className='restaurant'>
-        <div className='restaurant-picture' style={style} />
-        <div className='restaurant-name'>{name}</div>
+      <div className='restaurant' onClick={this.props.handleClick}>
+        <div className='restaurant-lunchspots'>
+          <div className='restaurant-name'>{restaurant}</div>
+          <div className='restaurant-address'>{address} </div>
+          <div className='restaurant-feedback'>{feedback} </div>
+          <div className='restaurant-feedback'>{ambiance} </div>
+          <div className='restaurant-feedback'>{crowds} </div>
+          <div className='restaurant-feedback'>{deals} </div>
+        </div>
+        <div className='restaurant-happyhours'>
+          <div className='restaurant-name'>{restaurant}</div>
+          <div className='restaurant-address'>{address} </div>
+          <div className='restaurant-feedback'>{feedback} </div>
+          <div className='restaurant-feedback'>{ambiance} </div>
+          <div className='restaurant-feedback'>{crowds} </div>
+          <div className='restaurant-feedback'>{deals} </div>
+        </div>
       </div>
     )
   }
 }
 
 export default Restaurant
+
