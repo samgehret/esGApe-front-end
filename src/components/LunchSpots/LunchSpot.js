0b19db1424596dd2972413cf9fa390e093c3ea47
Sam Gehret <sam.gehret@gmail.com>
2018-03-28 15:31:08 -0400

    added comment form

--- src/components/LunchSpots/LunchSpot.js
+++ src/components/LunchSpots/LunchSpot.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react'
+import Comment from '../forms/Comment'
 import axios from 'axios'
 
 class LunchSpot extends Component {
   constructor () {
     super()
     this.state = {
-      info: []
+      info: [],
+      comments: [],
+      comment: ''
     }
+    this.handleComment = this.handleComment.bind(this)
+    this.submitComment = this.submitComment.bind(this)
+    this.getInfo = this.getInfo.bind(this)
   }
   componentDidMount () {
+    this.getInfo()
+  }
+
+  getInfo () {
     axios.get(`http://localhost:3002/lunchspots/${this.props.match.params.id}`)
-          .then((res) => {
-            this.setState({info: res.data})
-            console.log('state')
-            console.log(this.state.info)
-          })
+    .then((res) => {
+      this.setState({info: res.data, comments: res.data.comments})
+      // console.log('state')
+      // console.log(this.state.info._id)
+    })
+  }
+  handleComment (e) {
+    this.setState({comment: e.target.value})
+    console.log(this.state.comment)
+  }
+  submitComment (e) {
+    e.preventDefault()
+    axios.put(`http://localhost:3002/lunchspots/comments/${this.state.info._id}`, {comment: this.state.comment})
+    let newComments = this.state.comments.slice()
+    console.log('newcomments')
+    console.log(newComments)
+    newComments.push({'commentText': this.state.comment})
+    this.setState({comments: newComments})
   }
   render (props) {
-    // console.log('individual info')
-    // console.log(this.props.match.params.id)
+    var comments = this.state.comments.map((comment, i) => {
+      return (
+        <div >
+          {comment.commentText}
+        </div>
+      )
+    })
     return (
       <div>
         <h1>Individual LunchSpot</h1>
@@ -34,6 +62,10 @@ class LunchSpot extends Component {
         <p>{this.state.info.foodType}</p>
         <p>{this.state.info.crowds}</p>
         <p>{this.state.info.ambiance}</p>
+        <h3>Comments</h3>
+        {comments}
+        <Comment handleComment={this.handleComment} submitComment={this.submitComment} />
+
       </div>
     )
   }
