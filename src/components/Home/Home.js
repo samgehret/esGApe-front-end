f6bce48bb6ae34c0e3a70ccf8c3f1a0152b5c8a5
Sam Gehret <sam.gehret@gmail.com>
2018-03-28 11:36:43 -0400

    updated links on homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './Home.css'
 
 class Home extends Component {
@@ -8,12 +9,16 @@ class Home extends Component {
         <h1>Welcome to EsGApe!</h1>
         <h2>What are you looking to do</h2>
         <div className='container'>
-          <div className='clickbox'>
-            <h3>Eat Lunch</h3>
-          </div>
-          <div className='clickbox'>
-            <h3>Go to Happy Hour</h3>
-          </div>
+          <Link to='/lunchspots'>
+            <div className='clickbox'>
+              <h3>Eat</h3>
+            </div>
+          </Link>
+          <Link to='/happyhours'>
+            <div className='clickbox'>
+              <h3>Drink</h3>
+            </div>
+          </Link>
         </div>
       </div>
     )
