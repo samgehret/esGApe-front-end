a335f0ce97c05e2ec97230447f745c8d89ae93e3
Sam Gehret <sam.gehret@gmail.com>
2018-03-28 12:05:10 -0400

    updated home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,12 +13,22 @@ class Home extends Component {
         <div className='container'>
           <Link to='/lunchspots'>
             <div className='clickbox'>
-              <h3>Eat</h3>
+              <div>
+                <h3>Eat</h3>
+              </div>
+              <div>
+                <img src='https://i.giphy.com/media/eSQKNSmg07dHq/200.gif' />
+              </div>
             </div>
           </Link>
           <Link to='/happyhours'>
             <div className='clickbox'>
-              <h3>Drink</h3>
+              <div>
+                <h3>Drink</h3>
+              </div>
+              <div>
+                <img src='https://i.giphy.com/media/IRoz0jP2Pr26I/giphy.webp' />
+              </div>
             </div>
           </Link>
         </div>
