e7da0fa79b42ca05a2b77f3ccf932e20158e88c7
SAMUEL HAILE <samishken@gmail.com>
2018-03-28 17:11:33 -0400

    updated happyhour

--- src/components/HappyHours/HappyHours.js
+++ src/components/HappyHours/HappyHours.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
-//import './Happyhours.css'
+import './HappyHours.css'
 
 class Happyhours extends Component {
   constructor (props) {
