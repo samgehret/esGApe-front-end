6a99b9eef3ca6b18180add50a9920c3a7542689c
Grant_Nathaniel_Brown <gnb225@nyu.edu>
2018-03-29 18:38:00 -0400

    fuck

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Route, Link, Redirect, Switch } from 'react-router-dom'
 import './Navbar.css'
 
 class Navbar extends Component {
-  render () {
+    render() {
         // this code could be a LOT cleaner
         // This if statement makes it so that it only renders certain nav
         // options when the user is logged in
-<<<<<<< HEAD
         if (this.props.isLoggedIn === false) {
             return (
                 <div>
@@ -26,10 +25,10 @@ class Navbar extends Component {
                     <nav className='nav'>
                         <Link className='nav-link' to='/'><h3>esGApe</h3></Link>
                         <img src="https://image.flaticon.com/icons/svg/168/168570.svg" />
-                        <Link className='nav-link' to='/happyhours'>Happy Hours</Link>
+                        <Link className='nav-link wine' to='/happyhours'>Happy Hours</Link>
                         <img src="https://image.flaticon.com/icons/svg/168/168570.svg" />
                         <img src="https://image.flaticon.com/icons/svg/235/235830.svg" />
-                        <Link className='nav-link' to='/lunchspots'>Lunch Spots</Link>
+                        <Link className='nav-link plate' to='/lunchspots'>Lunch Spots</Link>
                         <img src="https://image.flaticon.com/icons/svg/235/235830.svg" />
                         <Link className='nav-link' to='/newhappyhour'>Add New Happy Hour</Link>
                         <Link className='nav-link' to='/newlunchspot'>Add New Lunch Spot</Link>   
@@ -38,35 +37,7 @@ class Navbar extends Component {
                 </div>
             )
         }
-=======
-    if (this.props.isLoggedIn === false) {
-      return (
-        <div>
-          <nav className='nav'>
-            <Link className='nav-link' to='/'>esGApe home</Link>
-            <Link className='nav-link' to='/signup'>Signup</Link>
-            <Link className='nav-link' to='/login'>Login</Link>
-            <Link className='nav-link' to='/lunchspots'>Lunch Spots</Link>
-            <Link className='nav-link' to='/happyhours'>Happy Hours</Link>
-          </nav>
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          <nav className='nav'>
-            <Link className='nav-link' to='/'>esGApe home</Link>
-            <Link className='nav-link' to='/happyhours'>Happy Hours</Link>
-            <Link className='nav-link' to='/lunchspots'>Lunch Spots</Link>
-            <Link className='nav-link' to='/newhappyhour'>Add New Happy Hour</Link>
-            <Link className='nav-link' to='/newlunchspot'>Add New Lunch Spot</Link>
-            <input value='Log Out' type='submit' onClick={this.props.handleLogOut} />
-          </nav>
-        </div>
-      )
->>>>>>> develop
     }
-  }
 }
 
-export default Navbar
+export default Navbar;
